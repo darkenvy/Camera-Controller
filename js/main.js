1b158e310aa522d18d1ef5b2d4dda977b5d43f62
darkenvy <darkenvy6@gmail.com>
2016-08-08 17:44:15 -0700

    Post Panic. Camera is now micro. Need to recalibrate capture colors

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 // ------------------ Immediate Functions ---------------------- //
 // Need to init rearCamera ASAP
-var rearCam;
-(function rearCamera() {
-  console.log('inside rear camera')
-  MediaStreamTrack.getSources(function(s) {
-    // MODIFIED - Manually specified 1 as the rear camera for the S7. Should default to normal for desktop testing
-    rearCam = s[1].id;
-  })
-})()
+// var rearCam;
+// (function rearCamera() {
+//   console.log('inside rear camera')
+//   MediaStreamTrack.getSources(function(s) {
+//     // MODIFIED - Manually specified 1 as the rear camera for the S7. Should default to normal for desktop testing
+//     rearCam = s[1].id;
+//   })
+// })()
 
 
 
@@ -44,6 +44,7 @@ window.onload = function() {
       }
       return false;
     })
+    // tracker = new tracking.ColorTracker([]);
     tracker = new tracking.ColorTracker(['green', 'orange']);
     tracker['minDimension'] = 5;
     tracker['maxDimension'] = 100;
@@ -66,60 +67,82 @@ window.onload = function() {
   
   function initCamera() {
     tracking.track('#video', tracker, { camera: true });
+    var isReady = true;
+    setInterval(function() {
+      isReady = true;
+    }, 10)
+
     tracker.on('track', function(event) {
-      context.clearRect(0, 0, canvas.width, canvas.height);
+      if (isReady) {
+        isReady = false;
+        context.clearRect(0, 0, canvas.width, canvas.height);
 
-      // Multi-target tracking
-      event.data.forEach(function(rect) {
-        if (rect.color === 'custom') {rect.color = tracker.customColor;}
-        context.strokeStyle = rect.color;
-        context.strokeRect(rect.x, rect.y, rect.width, rect.height);
-        context.font = '11px Helvetica';
-        context.fillStyle = "#fff";
-        context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
-        context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
-        // angleGun(rect.x)
-      });
+        // Multi-target tracking
+        event.data.forEach(function(rect) {
 
-      if (event.data.length >= 2) {
-        // Set vars equal to first two color results
-        var color1 = event.data[0].color,
-            color2 = event.data[1].color
-
-        // Test to ensure that the 2 colors are only one of each.
-        // ie: not 'green green' or 'orange orange'. aka: false positives
-        if (color1 === 'orange' && color2 === 'green') {
-          angleGun(event.data[1].x, event.data[1].y, event.data[0].x, event.data[0].y)
-        } else if (color1 === 'green' && color2 === 'orange') {
-          angleGun(event.data[0].x, event.data[0].y, event.data[1].x, event.data[1].y)
-          // Green needs to be first in the execution of angleGun. 
-          // Green determines positioning
+          // if (rect.color === 'custom') {rect.color = tracker.customColor;}
+          context.strokeStyle = rect.color;
+          context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+          // context.font = '11px Helvetica';
+          // context.fillStyle = "#fff";
+          // context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
+          // context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+          // angleGun(rect.x)
+        });
+        if (event.data.length >= 1) {
+          if (event.data[0].color === 'green') {
+            positionGun(event.data[0].x, event.data[0].y)
+          } else if (event.data.length >= 2 && event.data[1].color === 'green') {
+            positionGun(event.data[1].x, event.data[1].y)
+          }
         }
+        if (event.data.length >= 2) {
+          // Set vars equal to first two color results
+          var color1 = event.data[0].color,
+              color2 = event.data[1].color
 
+          // Test to ensure that the 2 colors are only one of each.
+          // ie: not 'green green' or 'orange orange'. aka: false positives
+          if (color1 === 'orange' && color2 === 'green') {
+            angleGun(event.data[1].x, event.data[1].y, event.data[0].x, event.data[0].y)
+          } else if (color1 === 'green' && color2 === 'orange') {
+            angleGun(event.data[0].x, event.data[0].y, event.data[1].x, event.data[1].y)
+            // Green needs to be first in the execution of angleGun. 
+            // Green determines positioning
+          }
+
+        }
       }
 
     });
   }
+  function positionGun(x,y) {
+    var posX = (x / 320) - 0.5, // Only want to adjust betweet half a meter in-game
+        posY = 0-((y / 240) - 0.5) + 1, // Y is elevation in this case
+        posZ = -1 // 
 
+    document.getElementById('gun').setAttribute('position', posX + ' ' + posY + ' ' + posZ)
+  }
   function angleGun(x,y, x2, y2) {
     // see https://en.wikipedia.org/wiki/Aircraft_principal_axes for defenitions
     // ALERT - These divide-by numbers will change based on size of camera frame
     var pitch = Math.abs(y - y2) / 2.67, // elevation diff
         yaw = (x - x2) / 2, // left right diff
-        roll = 0, // roll not needed atm
-        posX = (x / 320) - 0.5, // Only want to adjust betweet half a meter in-game
-        posY = 0-((y / 240) - 0.5) + 1, // Y is elevation in this case
-        posZ = -1 // 
-
+        roll = 0 // roll not needed atm
 
     document.getElementById('gun').setAttribute('rotation', pitch + ' ' + yaw + ' ' + roll)
-    document.getElementById('gun').setAttribute('position', posX + ' ' + posY + ' ' + posZ)
   }
   // ------------------ Main initialization ----------------- //
   // colorset(); // will call setcolors() and initcamera() on its own
+  
   setColors();
   initCamera(); // Start the camera up now that we have the colors defined
-
+  // setTimeout(function() {document.getElementById('video').setAttribute('style', 'display: none');}, 2000)
+  
+  // setInterval(function() {
+  //   // setColors();
+  //   initCamera();
+  // }, 5000)
 
   // Turn on the Color controller (seems to be required)
   // initGUIControllers(tracker);
